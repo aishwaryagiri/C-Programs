f8ef54d00d305f30450fddd38df1a1259126ce8b
aishwaryagiri <82766057+aishwaryagiri@users.noreply.github.com>
2025-07-06 12:08:50 +0000

    s

--- Hello.c
+++ Hello.c
@@ -1,5 +1,15 @@
-# include < stdio.h>
-void main ()
+#include <stdio.h>
+#include <stdint.h>
+
+void main()
 {
-printf("Hello World");
-}
+   int c = 3;
+    int *cptr = &c;
+    int i = 0xff;
+    uintptr_t castptr = (uintptr_t)castptr;
+int m = castptr & i ;
+printf("uintptr_t castptr = %lu\n",castptr );
+printf("uintptr_t castptr = 0x%lx\n",castptr);
+printf("m = 0x%x\n",m);
+   
+}       
