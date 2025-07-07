91c1d698c8332d556d53ae75065b9b65eea442bd
aishwaryagiri <82766057+aishwaryagiri@users.noreply.github.com>
2025-07-07 06:54:52 +0000

    address address of pointer variable and comment

--- stringPointer.c
+++ stringPointer.c
@@ -3,6 +3,8 @@ void main(){
   char *name = "AISH";
   int p =10;
   int *m = &p;
-  printf("name = %c",*name);
-  printf("m = %p",m);
+  printf("*name = %c\n",*name);
+  printf("(void*)m = %p\n",(void*)m);//this will give the same result as the below line of code, still we should typecast the pointer to (void*) type for prtability
+  printf("m = %p\n",m);
+  printf("address of pointer variable m =%p",&m);
 }
