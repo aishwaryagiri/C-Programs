b0758543f17bffc5077cbcb3a17c7090977e03ff
aishwaryagiri <82766057+aishwaryagiri@users.noreply.github.com>
2025-07-07 07:02:10 +0000

    best practice added and code corrected

--- typecastPointer.c
+++ typecastPointer.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void main()
+int main(void) // its a best practice to use void inside the main function, it makes the function portable.
 {
    int c = 3;
     int *cptr = &c;
     int i = 0xff;
-    uintptr_t castptr = (uintptr_t)castptr;
+    uintptr_t castptr = (uintptr_t)cptr;
 int m = castptr & i ;
 printf("uintptr_t castptr = %lu\n",castptr );
 printf("uintptr_t castptr = 0x%lx\n",castptr);
+printf("cptr = %p\n",(void*)cptr);
 printf("m = 0x%x\n",m);
-   
+ return 0;  
 }       
