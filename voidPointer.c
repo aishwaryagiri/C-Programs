0409a6164f0c075f067c7f2263b8045cce2ea54d
aishwaryagiri <82766057+aishwaryagiri@users.noreply.github.com>
2025-07-09 11:18:31 +0000

    staged

--- voidPointer.c
+++ voidPointer.c
@@ -3,6 +3,7 @@ int main()
 {
     int a =12;
     void *b =(int*) &a;
-    printf("b = %d",b);
+    //printf("b = %d",b);//This will give compilation error void* type pointer cannot be dereferenced. we must typecast them before dereferencing//
+    printf("b = %d",*(int*)b); //we have typecasted the void pointer to int type//
     return 0;
 }
